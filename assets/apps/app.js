7327a4308ead255ad6c814915b4d0d87d3893ad8
Chris Laughter <chrislaugh@gmail.com>
2022-05-04 18:29:06 -0600

    create about me modal

---
+++ assets/apps/app.js
@@ -0,0 +1,18 @@
+const oMBtn = document.querySelector("#open-modal-btn");
+const cMBtn = document.querySelector("#close-modal-btn");
+const mod = document.querySelector("#modal");
+const oL = document.querySelector("#overlay");
+
+oMBtn.addEventListener("click", () => {
+  mod.classList.add("open");
+  oL.classList.add("open");
+});
+
+cMBtn.addEventListener("click", closeModal);
+
+oL.addEventListener("click", closeModal);
+
+function closeModal() {
+  mod.classList.remove("open");
+  oL.classList.remove("open");
+}
