499c6d70968faf36e639bb5acd333de42a046637
Chris Laughter <chrislaugh@gmail.com>
2022-05-09 19:27:38 -0600

    bring modal inside page without having to click for outside page

--- assets/apps/app.js
+++ assets/apps/app.js
@@ -1,20 +1,15 @@
-const oMBtn = document.querySelector("#open-modal-btn");
-const cMBtn = document.querySelector(".close-button");
-const cModBtn = document.querySelector("#close-modal-btn");
-const mod = document.querySelector("#modal");
-const oL = document.querySelector("#overlay");
-
-oMBtn.addEventListener("click", () => {
-  mod.classList.add("open");
-  oL.classList.add("open");
+const aboutMe = document.querySelectorAll(".sub-title");
+const modal = document.querySelector(".modal");
+const backdrop = document.querySelector(".backdrop");
+const textBody = document.querySelectorAll(".text-bodyModal");
+const closeHome = document.querySelectorAll("#close-modal-btn");
+// This is where we want to open the modal
+aboutMe[0].addEventListener("click", () => {
+  modal.style.display = "block";
+  backdrop.style.display = "block";
 });
 
-cMBtn.addEventListener("click", closeModal);
-cModBtn.addEventListener("click", closeModal);
-
-oL.addEventListener("click", closeModal);
-
-function closeModal() {
-  mod.classList.remove("open");
-  oL.classList.remove("open");
-}
+closeHome[0].addEventListener("click", () => {
+  modal.style.display = "none";
+  backdrop.style.display = "none";
+});
